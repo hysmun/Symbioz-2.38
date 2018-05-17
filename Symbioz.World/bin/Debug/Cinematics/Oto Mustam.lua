54275fc1ff6ee04f725cf468647d146a28bcd54d
joaoggs <joaogabrielggs@gmail.com>
2018-05-17 19:48:11 -0300

    Npcs moved to brak

---
+++ Symbioz.World/bin/Debug/Cinematics/Oto Mustam.lua
@@ -0,0 +1,29 @@
+
+
+
+ -- Script De cinématique.
+---------
+author = "Skinz"
+modified = "Joaoggs"
+mapId = 144419
+npcId = 107
+doneObjectives = {}
+notDoneObjectives = {} 
+criteria = ""
+--------
+
+function TalkToNpc()
+
+env:sayNpc("Welcome adventurous, if you need items talk to the npcs !! For <b>Level UP say .up</b>");
+
+
+
+end
+
+function Execute()
+
+end
+
+function CriteriaWrong()
+
+end
